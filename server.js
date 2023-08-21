eb50a3d781d30328d29ce28a8710e08637118eab
prateek gupta <pateek123@gmail.com>
2023-08-21 23:57:52 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+import {app} from "./app.js";
+import {connectDB} from "./data/database.js";
+
+connectDB();
+
+console.log(process.env.PORT)
+
+app.listen(process.env.PORT,()=>{
+    console.log(`server is working on ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+})
